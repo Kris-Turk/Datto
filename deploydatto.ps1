36898e0cba4623c2b3747faf4167192fd5b3398c
Kris Turk <kris.turk@hybrit.co.nz>
2022-02-14 14:38:14 +1300

    edited script to create directory

--- deploydatto.ps1
+++ deploydatto.ps1
@@ -2,12 +2,13 @@
 $appName = 'Datto'
 $drive = 'C:\'
 New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
-$LocalPath = $drive + '\AppInstalls\' + $appName 
+$LocalPath = $drive + '\AppInstalls\' + $appName
+mkdir $LocalPath
 set-Location $LocalPath
 $URL = 'https://raw.githubusercontent.com/Kris-Turk/Datto/main/tmp/AgentSetup_The%2BMarketing%2BPractice.exe'
 $URLexe = 'DattoWindowsAgent.exe'
 $outputPath = $LocalPath + '\' + $URLexe
 Invoke-WebRequest -Uri $URL -OutFile $outputPath
-write-host 'Starting Install  KeePass'
+write-host 'Starting Install  Datto'
 Start-Process -FilePath $outputPath -Args "/quiet /norestart " -Wait
 write-host 'Finished Install the of Datto'
--- vmRunScript.ps1
+++ vmRunScript.ps1
@@ -1,9 +1,12 @@
 $scriptURI = 'https://raw.githubusercontent.com/Kris-Turk/Datto/main/deploydatto.ps1'
-$runCommandName = 'DeployDatto'
-
 $vms = Get-AzVM | Select-Object ResourceGroupName,Name,Location
+$runCommandName = 'DattoDeploy'
 
 ForEach ($vm in $vms) {
-    Set-AzVMRunCommand -ResourceGroupName $vm.ResourceGroupName -VMName $vm.Name -RunCommandName $runCommandName -Location $vm.Location -SourceScriptURI $scriptURI
+    Set-AzVMRunCommand -ResourceGroupName $vm.ResourceGroupName `
+    -VMName $vm.Name `
+    -RunCommandName $runCommandName `
+    -Location $vm.Location `
+    -SourceScriptURI $scriptURI
 }
 
